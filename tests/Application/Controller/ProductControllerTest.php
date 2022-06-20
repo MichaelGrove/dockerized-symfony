ebad888a7ea66f48981369bd530b0186def03ff8
Mikael Lehto <mikael.leh@hotmail.com>
2022-06-20 10:38:14 +0300

    made services public

--- tests/Application/Controller/ProductControllerTest.php
+++ tests/Application/Controller/ProductControllerTest.php
@@ -6,17 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
 {
-    public function testProductCanBeCreated(): void
-    {
-        $client = static::createClient();
+  public function testProductCanBeCreated(): void
+  {
+    $client = static::createClient();
 
-        $client->request('POST', '/product');
+    $client->request('POST', '/product');
 
-        $this->assertResponseIsSuccessful();
-        $content = $client->getResponse()->getContent();
-        $data = json_decode($content, true);
-        $this->assertNotNull($data['id']);
-        $this->assertSame('Bicycle', $data['name']);
-        $this->assertSame('300.99', $data['price']);
-    }
+    $this->assertResponseIsSuccessful();
+    $content = $client->getResponse()->getContent();
+    $data = json_decode($content, true);
+    $this->assertNotNull($data['id']);
+    $this->assertSame('Bicycle', $data['name']);
+    $this->assertSame('300.99', $data['price']);
+  }
 }
